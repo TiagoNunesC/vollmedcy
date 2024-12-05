d7beace18c15122db271b05793d9d39c3bd74fc3
Tiago Nunes <tiagoo2001@yahoo.com.br>
2024-12-05 14:24:20 -0300

    Atualizando Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,33 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import cypress from "eslint-plugin-cypress";
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    ignores: [
+      "node_modules/",
+      "build/",
+      "dist/",
+      "*.min.js",
+      "coverage/",
+    ],
+    files: ["cypress/**/*.cy.js"], // Aplica apenas nos arquivos de teste
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        cy: "readonly",
+        Cypress: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        context: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+      },
+    },
+    plugins: {
+      cypress,
+    },
+    rules: {
+      // Suas regras personalizadas aqui
+    },
+  },
 ];
